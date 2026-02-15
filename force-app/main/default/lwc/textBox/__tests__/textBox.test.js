46664cff9cfccf22533c5ba068ad6b71362a3b90
IZEICG-LuisaMorales <lmorales@izeicg.com>
2026-02-15 00:14:39 -0700

    prettier fix

--- force-app/main/default/lwc/textBox/__tests__/textBox.test.js
+++ force-app/main/default/lwc/textBox/__tests__/textBox.test.js
@@ -1,76 +1,143 @@
-import { createElement } from '@lwc/engine-dom';
-import TextBox from 'c/textBox';
-import getMessage from '@salesforce/apex/TwilioController.getMessage';
+import { createElement } from "@lwc/engine-dom";
+import TextBox from "c/textBox";
+import getMessage from "@salesforce/apex/TwilioController.getMessage";
 
 jest.mock(
-    '@salesforce/apex/TwilioController.getMessage',
-     () => {
-        return {
-            default: jest.fn()
-        };
-    },
-    { virtual: true }
+  "@salesforce/apex/TwilioController.getMessage",
+  () => {
+    return {
+      default: jest.fn()
+    };
+  },
+  { virtual: true }
 );
 
 // Helper function to wait until the microtask queue is empty. This is needed for promise
 // timing when calling imperative Apex.
 async function flushPromises() {
-    return Promise.resolve();
+  return Promise.resolve();
 }
 
-describe('c-text-box', () => {
-    afterEach(() => {
-        while (document.body.firstChild) {
-            document.body.removeChild(document.body.firstChild);
-        }
-        jest.clearAllMocks();
+describe("c-text-box", () => {
+  afterEach(() => {
+    while (document.body.firstChild) {
+      document.body.removeChild(document.body.firstChild);
+    }
+    jest.clearAllMocks();
+  });
+
+  it("llama a Apex y muestra el mensaje con el nombre cuando el usuario da clic", async () => {
+    const element = createElement("c-text-box", {
+      is: TextBox
     });
+    document.body.appendChild(element);
 
-    it('llama a Apex y muestra el mensaje cuando el usuario da clic', async () => {
-        const element = createElement('c-text-box', {
-            is: TextBox
-        });
-        document.body.appendChild(element);
+    const input = element.shadowRoot.querySelectorAll("lightning-input")[0];
 
-        const RESPUESTA_ESPERADA = 'Hola, Pedro';
-        getMessage.mockResolvedValue(RESPUESTA_ESPERADA);
+    const RESPUESTA_ESPERADA = "Hola, Pedro";
+    getMessage.mockResolvedValue(RESPUESTA_ESPERADA);
 
-        const input = element.shadowRoot.querySelector('lightning-input');
-        input.dispatchEvent(new CustomEvent('change', { detail: { value: 'Pedro' } }));
-        
-        await flushPromises();
+    input.dispatchEvent(
+      new CustomEvent("change", { detail: { value: "Pedro" } })
+    );
 
-        const button = element.shadowRoot.querySelector('lightning-button');
-        button.click();
+    await flushPromises();
 
-        await flushPromises();
-        await flushPromises();
-        
-        expect(getMessage).toHaveBeenCalledWith({ name: 'Pedro' });
-        const textElements = element.shadowRoot.querySelectorAll('lightning-formatted-text');
-        
-        const responseText = textElements[textElements.length - 1];
-        expect(responseText.value).toBe(RESPUESTA_ESPERADA);
+    const button = element.shadowRoot.querySelector("lightning-button");
+    button.click();
 
-        const inputElement = element.shadowRoot.querySelector('lightning-input');
-        expect(inputElement.value).toBe('');
+    await flushPromises();
+    await flushPromises();
+
+    expect(getMessage).toHaveBeenCalledWith({ name: "Pedro", age: 0 });
+    const textElements = element.shadowRoot.querySelectorAll(
+      "lightning-formatted-text"
+    );
+
+    const responseText = textElements[textElements.length - 1];
+    expect(responseText.value).toBe(RESPUESTA_ESPERADA);
+
+    // const inputElement = element.shadowRoot.querySelector('lightning-input');
+    expect(input.value).toBe("");
+  });
+  it("llama a Apex y muestra el mensaje con el nombre y la edadcuando el usuario da clic", async () => {
+    const element = createElement("c-text-box", {
+      is: TextBox
     });
+    document.body.appendChild(element);
 
-    it('muestra un error si el input está vacío y NO llama a Apex', async () => {
-        const element = createElement('c-text-box', {
-            is: TextBox
-        });
-        document.body.appendChild(element);
+    const nameInput = element.shadowRoot.querySelectorAll("lightning-input")[0];
+    const ageInput = element.shadowRoot.querySelectorAll("lightning-input")[1];
 
-        const toastHandler = jest.fn();
-        element.addEventListener('lightning__showtoast', toastHandler);
+    nameInput.dispatchEvent(
+      new CustomEvent("change", { detail: { value: "Pedro" } })
+    );
+    ageInput.dispatchEvent(
+      new CustomEvent("change", { detail: { value: 20 } })
+    );
 
-        const button = element.shadowRoot.querySelector('lightning-button');
-        button.click();
+    const RESPUESTA_ESPERADA =
+      "Hola, Pedro, te recuerdo que ya casi tienes 21 años!";
+    getMessage.mockResolvedValue(RESPUESTA_ESPERADA);
 
-        await flushPromises();
+    await flushPromises();
 
-        expect(getMessage).not.toHaveBeenCalled();
-        expect(toastHandler).toHaveBeenCalled();
+    const button = element.shadowRoot.querySelector("lightning-button");
+    button.click();
+
+    await flushPromises();
+    await flushPromises();
+
+    expect(getMessage).toHaveBeenCalledWith({ name: "Pedro", age: 20 });
+    const textElements = element.shadowRoot.querySelectorAll(
+      "lightning-formatted-text"
+    );
+
+    const responseText = textElements[textElements.length - 1];
+    expect(responseText.value).toBe(RESPUESTA_ESPERADA);
+
+    // const inputElement = element.shadowRoot.querySelector('lightning-input');
+    expect(nameInput.value).toBe("");
+    expect(ageInput.value).toBe(0);
+  });
+
+  it("muestra un error si el input está vacío y NO llama a Apex", async () => {
+    const element = createElement("c-text-box", {
+      is: TextBox
     });
+    document.body.appendChild(element);
+
+    const toastHandler = jest.fn();
+    element.addEventListener("lightning__showtoast", toastHandler);
+
+    const button = element.shadowRoot.querySelector("lightning-button");
+    button.click();
+
+    await flushPromises();
+
+    expect(getMessage).not.toHaveBeenCalled();
+    expect(toastHandler).toHaveBeenCalled();
+  });
+
+  it("muestra un error si el nombre está vacío pero la edad no y NO llama a Apex", async () => {
+    const element = createElement("c-text-box", {
+      is: TextBox
+    });
+    document.body.appendChild(element);
+
+    const input = element.shadowRoot.querySelectorAll("lightning-input")[1];
+
+    const toastHandler = jest.fn();
+    element.addEventListener("lightning__showtoast", toastHandler);
+
+    input.dispatchEvent(new CustomEvent("change", { detail: { value: 20 } }));
+
+    const button = element.shadowRoot.querySelector("lightning-button");
+    button.click();
+
+    await flushPromises();
+
+    expect(getMessage).not.toHaveBeenCalled();
+    expect(toastHandler).toHaveBeenCalled();
+  });
 });
--- force-app/main/default/lwc/textBox/textBox.js
+++ force-app/main/default/lwc/textBox/textBox.js
@@ -1,38 +1,44 @@
-import { LightningElement } from 'lwc';
-import getMessage from '@salesforce/apex/TwilioController.getMessage';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { LightningElement } from "lwc";
+import getMessage from "@salesforce/apex/TwilioController.getMessage";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
 export default class TextBox extends LightningElement {
-    userName='';
-    message;
+  userName = "";
+  message;
+  userAge = 0;
 
-    handleOnChange(event) {
-        this.userName = event.detail.value;
-    }
+  handleOnChange(event) {
+    this.userName = event.detail.value;
+  }
 
-    handleClick(){
-        if (!this.userName) {
-            this.showToast('Error', 'Please enter a name', 'warning');
-            return;
-        }
-        getMessage({name:this.userName})
-            .then(resp=>{
-                this.message = resp;
-                this.userName='';
-                this.showToast('Success', 'Message received!', 'success');
-            })
-            .catch(error=>{
-                console.error(error);
-                this.showToast('Error', 'Something went wrong', 'error');
-            });
-    }
+  handleAgeOnChange(event) {
+    this.userAge = event.detail.value;
+  }
 
-    showToast(title, message, variant) {
-        const event = new ShowToastEvent({
-            title: title,
-            message: message,
-            variant: variant
-        });
-        this.dispatchEvent(event);
+  handleClick() {
+    if (!this.userName) {
+      this.showToast("Error", "Please enter a name", "warning");
+      return;
     }
+    getMessage({ name: this.userName, age: this.userAge })
+      .then((resp) => {
+        this.message = resp;
+        this.userName = "";
+        this.userAge = 0;
+        this.showToast("Success", "Message received!", "success");
+      })
+      .catch((error) => {
+        console.error(error);
+        this.showToast("Error", "Something went wrong", "error");
+      });
+  }
+
+  showToast(title, message, variant) {
+    const event = new ShowToastEvent({
+      title: title,
+      message: message,
+      variant: variant
+    });
+    this.dispatchEvent(event);
+  }
 }
